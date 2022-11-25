24b8284d9e6cdc6bc07b1370e32f4a4fa37512b2
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-11-25 19:43:33 +0100

    my solution of the challenge #4

--- script.js
+++ script.js
@@ -116,235 +116,257 @@
 
 // Challenge 2
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-// 1.1 solution
-// for (let i = 0; i < game.scored.length; i++) {
-//   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// // 1.1 solution
+// // for (let i = 0; i < game.scored.length; i++) {
+// //   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
+// // }
+
+// // 1.2 solution
+// for (const [goal, player] of game.scored.entries()) {
+//   console.log(`Goal ${goal + 1}: ${player}`);
 // }
 
-// 1.2 solution
-for (const [goal, player] of game.scored.entries()) {
-  console.log(`Goal ${goal + 1}: ${player}`);
-}
+// //2 solution
 
-//2 solution
+// let average = 0;
 
-let average = 0;
+// for (const odd of Object.values(game.odds)) {
+//   average += odd;
+// }
+// average /= Object.values(game.odds).length;
+// console.log(average);
 
-for (const odd of Object.values(game.odds)) {
-  average += odd;
-}
-average /= Object.values(game.odds).length;
-console.log(average);
+// //3 solution
 
-//3 solution
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr}: ${odd}`);
+// }
 
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
-  console.log(`Odd of ${teamStr}: ${odd}`);
-}
+// // Odd of victory Bayern Munich: 1.33
+// // Odd of draw: 3.25
+// // Odd of victory Borrussia Dortmund: 6.5
 
-// Odd of victory Bayern Munich: 1.33
-// Odd of draw: 3.25
-// Odd of victory Borrussia Dortmund: 6.5
+// //4 solution
 
-//4 solution
+// // So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+//   console.log(scorers[player]);
+// }
+// console.log(scorers);
 
-// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-  console.log(scorers[player]);
-}
-console.log(scorers);
+// // NOTES
 
-// NOTES
+// /*
 
-/* 
+// // Maps: Iteration
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎉'],
+//   [false, 'Try again!'],
+// ]);
+// console.log(question);
 
-// Maps: Iteration
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct 🎉'],
-  [false, 'Try again!'],
-]);
-console.log(question);
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-// Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// // Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
 
-// Quiz app
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
-}
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
 
-console.log(question.get(question.get('correct') === answer));
+// // Convert map to array
+// console.log([...question]);
+// // console.log(question.entries());
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 
-// Convert map to array
-console.log([...question]);
-// console.log(question.entries());
-console.log([...question.keys()]);
-console.log([...question.values()]);
+// */
 
+// // challenge #3
 
-*/
+// const gameEvents = new Map([
+//   [17, '⚽ GOAL'],
+//   [36, '� Substitution'],
+//   [47, '⚽ GOAL'],
+//   [61, '� Substitution'],
+//   [64, '� Yellow card'],
+//   [69, '� Red card'],
+//   [70, '� Substitution'],
+//   [72, '� Substitution'],
+//   [76, '⚽ GOAL'],
+//   [80, '⚽ GOAL'],
+//   [92, '� Yellow card'],
+// ]);
 
-// challenge #3
+// // 1.Create an array 'events' of the different game events that happened (no
+// // duplicates)
 
-const gameEvents = new Map([
-  [17, '⚽ GOAL'],
-  [36, '� Substitution'],
-  [47, '⚽ GOAL'],
-  [61, '� Substitution'],
-  [64, '� Yellow card'],
-  [69, '� Red card'],
-  [70, '� Substitution'],
-  [72, '� Substitution'],
-  [76, '⚽ GOAL'],
-  [80, '⚽ GOAL'],
-  [92, '� Yellow card'],
-]);
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
 
-// 1.Create an array 'events' of the different game events that happened (no
-// duplicates)
+// // 2. After the game has finished, is was found that the yellow card from minute 64
+// // was unfair. So remove this event from the game events log.
 
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+// gameEvents.delete(64);
+// console.log(gameEvents);
 
-// 2. After the game has finished, is was found that the yellow card from minute 64
-// was unfair. So remove this event from the game events log.
+// // 3. Compute and log the following string to the console: "An event happened, on
+// // average, every 9 minutes" (keep in mind that a game has 90 minutes)
 
-gameEvents.delete(64);
-console.log(gameEvents);
+// let numberEvent = 1;
 
-// 3. Compute and log the following string to the console: "An event happened, on
-// average, every 9 minutes" (keep in mind that a game has 90 minutes)
-
-let numberEvent = 1;
-
-for (const minute of gameEvents.keys()) {
-  minute < 90 ? numberEvent++ : numberEvent;
-}
-const averageMin = 90 / numberEvent;
-console.log(`An event happened, on
-average, every ${averageMin} minutes`);
+// for (const minute of gameEvents.keys()) {
+//   minute < 90 ? numberEvent++ : numberEvent;
+// }
+// const averageMin = 90 / numberEvent;
+// console.log(`An event happened, on
+// average, every ${averageMin} minutes`);
 
 // 4. Loop over 'gameEvents' and log each element to the console, marking
 // whether it's in the first half or second half (after 45 min) of the game, like this:
 // [FIRST HALF] 17: ⚽ GOAL
 
-for (const [min, event] of gameEvents) {
-  const half = min <= 45 ? 'FIRST' : 'SECOND';
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
 
-  console.log(`[${half} HALF] ${min}: ${event}`);
-}
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
 
-// Metody na String
+// // Metody na String
 
-const passenger = 'jOnAS';
-const correctName = firstName => {
-  const nameToLower = firstName.toLowerCase();
-  const firstLetterUpper = nameToLower[0].toUpperCase() + nameToLower.slice(1);
-  console.log(firstLetterUpper);
-};
+// const passenger = 'jOnAS';
+// const correctName = firstName => {
+//   const nameToLower = firstName.toLowerCase();
+//   const firstLetterUpper = nameToLower[0].toUpperCase() + nameToLower.slice(1);
+//   console.log(firstLetterUpper);
+// };
 
-correctName(passenger);
-correctName('iryNA');
-correctName('ILLia');
-correctName('MIa');
+// correctName(passenger);
+// correctName('iryNA');
+// correctName('ILLia');
+// correctName('MIa');
 
-// Comparing emails
-const email = 'hello@jonas.io';
-const loginEmail = '  Hello@Jonas.Io \n';
+// // Comparing emails
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@Jonas.Io \n';
 
-// const lowerEmail = loginEmail.toLowerCase();
-// const trimmedEmail = lowerEmail.trim();
-const normalizedEmail = loginEmail.toLowerCase().trim();
-console.log(normalizedEmail);
-console.log(email === normalizedEmail);
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
 
-// replacing
-const priceGB = '288,97£';
-const priceUS = priceGB.replace('£', '$').replace(',', '.');
-console.log(priceUS);
+// // replacing
+// const priceGB = '288,97£';
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+// console.log(priceUS);
 
-const announcement =
-  'All passengers come to boarding door 23. Boarding door 23!';
+// const announcement =
+//   'All passengers come to boarding door 23. Boarding door 23!';
 
-console.log(announcement.replace('door', 'gate'));
-// console.log(announcement.replaceAll('door', 'gate'));
-console.log(announcement.replace(/door/g, 'gate'));
+// console.log(announcement.replace('door', 'gate'));
+// // console.log(announcement.replaceAll('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
 
-// Booleans
-const plane = 'Airbus A320neo';
-console.log(plane.includes('A320'));
-console.log(plane.includes('Boeing'));
-console.log(plane.startsWith('Airb'));
+// // Booleans
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Airb'));
 
-if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
-  console.log('Part of the NEW ARirbus family');
-}
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('Part of the NEW ARirbus family');
+// }
 
-// Practice exercise
-const checkBaggage = function (items) {
-  const baggage = items.toLowerCase();
+// // Practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
 
-  if (baggage.includes('knife') || baggage.includes('gun')) {
-    console.log('You are NOT allowed on board');
-  } else {
-    console.log('Welcome aboard!');
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are NOT allowed on board');
+//   } else {
+//     console.log('Welcome aboard!');
+//   }
+// };
+
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and camera');
+// checkBaggage('Got some snacks and a gun for protection');
+
+// challenge 4
+
+// underscore_case
+//  first_name
+//  Some_Variable
+// calculate_AGE
+//  delayed_departur
+
+const input = document.body.appendChild(document.createElement('textarea'));
+const button = document.body.appendChild(document.createElement('button'));
+let wordsUpper;
+let number = 0;
+button.addEventListener('click', () => {
+  let words = input.value.split('\n');
+  for (const word of words) {
+    number++;
+    let [first, second] = word.trim().split('_');
+    second = second.replace(second[0], second[0].toUpperCase());
+    wordsUpper = [first, second].join('');
+    console.log(wordsUpper.padEnd(20, ' ') + '✅'.repeat(number));
   }
-};
-
-checkBaggage('I have a laptop, some Food and a pocket Knife');
-checkBaggage('Socks and camera');
-checkBaggage('Got some snacks and a gun for protection');
+});
